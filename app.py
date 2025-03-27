c5cfa2e295580186c8e62fb50c79bb99eaf22147
tempuser 02 <tempuser02@interlaceindia.local>
2025-03-27 16:50:46 +0530

    trainingmodel

--- app.py
+++ app.py
@@ -25,3 +25,18 @@ tokenizer.fit_on_texts(X_train)
 
 X_train_seq = pad_sequences(tokenizer.texts_to_sequences(X_train), maxlen=MAX_LEN, padding="post")
 X_test_seq = pad_sequences(tokenizer.texts_to_sequences(X_test), maxlen=MAX_LEN, padding="post")
+
+model = Sequential([
+    Embedding(MAX_WORDS, 128, input_length=MAX_LEN),
+    LSTM(64, return_sequences=True),
+    Dropout(0.3),
+    LSTM(32),
+    Dense(32, activation="relu"),
+    Dense(3, activation="softmax")  # 3 classes: Negative, Neutral, Positive
+])
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+model.summary()
+
+# Train model
+model.fit(X_train_seq, y_train, epochs=5, batch_size=32, validation_data=(X_test_seq, y_test))
